4b09aea1bf15de40b1fae6453aa9e764d2d6c914
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-10 14:00:04 +0700

    Menambahkan output dari algoritma pemrograman

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ public class Main {
         int tugas_1, uts_1, uas_1;
         int tugas_2, uts_2, uas_2;
         String nama, nim;
-        String hahahaha;
+        char huruf = 'A';
+        String Status = "Lulus";
+        int nilaiAkhir = 90;
 
         System.out.println("==== INPUT DATA MAHASISWA ====");
         System.out.print("Nama : ");
@@ -31,6 +33,8 @@ public class Main {
 
         System.out.println("Mata kuliah\t\tUTS\tUAS\tTugas\tNilai Akhir\tNilai Huruf\tStatus");
         System.out.println("-------------------------------------------------------------------");
+        System.out.println("Algoritma Pemrograman" + "\t" + uts_1 + "\t" + uas_1 + "\t\t" + tugas_1 + "\t" + nilaiAkhir + "\t" + huruf + "\t" + Status);
+        System.out.println("Struktur Data");
         BelajarNihBro();
         
     }
