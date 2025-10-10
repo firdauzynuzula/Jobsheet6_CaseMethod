cee2c5ac896c5298b02bdbb508108fc43d10f288
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-10 19:49:17 +0700

    Menambahkan function pada program

--- Main.java
+++ Main.java
@@ -5,9 +5,8 @@ public class Main {
         int tugas_1, uts_1, uas_1;
         int tugas_2, uts_2, uas_2;
         String nama, nim;
-        char huruf = 'A';
-        String Status = "Lulus";
-        int nilaiAkhir = 90;
+        String paraf = "BAIK";
+        String statusKelulusan = "BYUH";
 
         System.out.println("==== INPUT DATA MAHASISWA ====");
         System.out.print("Nama : ");
@@ -24,7 +23,18 @@ public class Main {
         System.out.print("Nilai Tugas  : ");
         tugas_1 = input.nextInt();
 
-        // Hasil atau output
+        System.out.println("--- Mata kuliah 2: Struktur Data ---");
+        System.out.print("Nilai UTS    : ");
+        uts_2 = input.nextInt();
+        System.out.print("Nilai UAS    : ");
+        uas_2 = input.nextInt();
+        System.out.print("Nilai Tugas  : ");
+        tugas_2 = input.nextInt();
+        input.close();
+
+        double nilaiAkhir_1 = (tugas_1 * 0.3) + (uts_1 * 0.3) + (uas_1 * 0.4);
+
+        // // Hasil atau output
         System.out.println("\n");
         System.out.println("========== HASIL PENILAIAN AKADEMIK ==========");
         System.out.println("Nama : " + nama);
@@ -33,14 +43,13 @@ public class Main {
 
         System.out.println("Mata kuliah\t\tUTS\tUAS\tTugas\tNilai Akhir\tNilai Huruf\tStatus");
         System.out.println("-------------------------------------------------------------------");
-        System.out.println("Algoritma Pemrograman" + "\t" + uts_1 + "\t" + uas_1 + "\t" + tugas_1 + "\t\t" + nilaiAkhir + "\t\t" + huruf + "\t" + Status);
-        System.out.println("Struktur Data" + "\t\t" + uts_1 + "\t" + uas_1 + "\t" + tugas_1 + "\t\t" + nilaiAkhir + "\t\t" + huruf + "\t" + Status);
-        
+        System.out.println("Algoritma Pemrograman" + "\t" + uts_1 + "\t" + uas_1 + "\t" + tugas_1 + "\t\t" + nilaiAkhir_1  + "\t\t" + paraf + "\t" + statusKelulusan);
+        System.out.println("Struktur Data" + "\t\t" + uts_2 + "\t" + uas_2 + "\t" + tugas_2 + "\t\t" + "\t\t" + nilaiAkhir_1 + paraf + "\t" + statusKelulusan);
     }
     
-    public static void BelajarNihBro() {
-        System.out.println("hello world");
+    public static void BelajarNihBro(int tugas_1, int uas_1, int uts_1) {
+        double hasil = (tugas_1 * 0.3) + (uts_1 * 0.3) + (uas_1 * 0.4);
+        System.out.println( hasil + "hehehe");
     }
-
 
 }
